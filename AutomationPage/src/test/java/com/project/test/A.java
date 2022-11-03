65ae47733629e6d25435cb2274d4e52e82ee9d21
Mahesh <mraghu810@gmail.com>
2022-11-03 20:09:20 +0530

    By me

---
+++ AutomationPage/src/test/java/com/project/test/A.java
@@ -0,0 +1,9 @@
+package com.project.test;
+
+public class A {
+	
+	public void m1() {
+		System.out.println("Hello");
+	}
+
+}
---
+++ AutomationPage/src/test/java/com/project/util/B.java
@@ -0,0 +1,9 @@
+package com.project.util;
+
+public class B {
+	
+	public void m2() {
+		System.out.println("Welcome to project");
+	}
+
+}
